86fffe713e9825a164ce9979714c913bdc7b5e45
DenysAptikieiev <rtdan333@gmail.com>
2019-11-20 10:31:15 +0200

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,65 @@
+"use strict";
+
+fetch('data.json')
+    .then(function (res) {
+        if (res.status === 200) return res.json();
+        console.log(`${res.status}: ${res.statusText}`);
+    })
+    .then(function (res) {
+        if (!localStorage.getItem('dataPhoto')) {
+            setLocalStorage(res, 'dataPhoto');
+            dataLocalPhoto = getLocalStorage('dataPhoto');
+        }
+    });
+
+
+    const slide = function() {
+        if (!isPaused) {
+            slideNumber++;
+            if (slideNumber >= dataLocalPhoto.length) slideNumber = 0;
+            $(`.slide-img`).attr('src', dataLocalPhoto[slideNumber].src);
+        } 
+    }
+$(document).ready(function () {
+
+    // setInterval(function () {
+        // dataLocalPhoto = getLocalStorage('dataPhoto');
+        // console.log(dataLocalPhoto)
+    // }, 1000);
+
+    // slider
+    let isPaused = false;
+
+    let autoSlide = setInterval(slide, 3000);
+
+    let slideNumber = 0;
+
+    $(`#arrowForward`).on('click', function () {
+        isPaused = true;
+
+        setTimeout(function () {
+            isPaused = false;
+        }, 5000);
+
+        slideNumber++;
+        if (slideNumber >= dataLocalPhoto.length) slideNumber = 0;
+        $(`.slide-img`).attr('src', dataLocalPhoto[slideNumber].src);
+    });
+
+    $(`#arrowBack`).on('click', function () {
+        isPaused = true;
+
+        setTimeout(function () {
+            isPaused = false;
+        }, 5000);
+
+        if (slideNumber === 0) slideNumber = (dataLocalPhoto.length);
+        slideNumber--;
+        $(`.slide-img`).attr('src', dataLocalPhoto[slideNumber].src);
+    });
+
+    //modal window
+    $(`#register, .closed`).on('click', function () {
+        $(`#fon`).toggleClass('hide');
+    });
+});
